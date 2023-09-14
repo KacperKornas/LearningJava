e6abe8e4686c8857cf8fcfc0b1d800355071e2be
Kacper Kornas <kornaskacper1@gmail.com>
2023-09-14 09:28:49 +0200

    Start first excercises

--- HelloWorld/src/SecondClass.java
+++ HelloWorld/src/SecondClass.java
@@ -1,5 +1,12 @@
 public class SecondClass {
-    public static void main(String[] args) {
-        System.out.print("Hello, Tim");
+        public static void main (String [] args) {
+        int number = -2;
+            if (number > 0) {
+                System.out.println("positive");
+            }    else if (number < 0) {
+                System.out.println("negative");
+            }   else {
+                System.out.println("zero");
+            }
+        }
     }
-}
