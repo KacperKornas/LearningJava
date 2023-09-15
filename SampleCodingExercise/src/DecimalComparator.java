af005b841a5a4ba4f290b521b072f8dc56d8f96a
KacperKornas <kornaskacper1@gmail.com>
2023-09-15 19:23:54 +0200

    DecimalComparator

--- SampleCodingExercise/src/DecimalComparator.java
+++ SampleCodingExercise/src/DecimalComparator.java
@@ -1,7 +1,15 @@
 public class DecimalComparator {
     public static void main(String[] args) {
-
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1776, -3.178));
     }
 
-    public static boolean areEqualByThreeDecimalPlaces(double)
+    public static boolean areEqualByThreeDecimalPlaces(double first, double second) {
+        double roundedFirstNumber = (int) (first * 1000.0) / 1000.0;
+        double roundedSecondNumber = (int) (second * 1000.0) / 1000.0;
+
+        if (roundedFirstNumber == roundedSecondNumber) {
+            return true;
+        }
+        return false;
+    }
 }
