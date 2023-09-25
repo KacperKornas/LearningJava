f9c90940dee8c2124463cebf3d4eae4de37d4062
Kacper Kornas <kornaskacper1@gmail.com>
2023-09-25 19:20:59 +0200

    Problem with leap year

---
+++ Section6ControlFlow/LearnWithCodeChallenge/src/NumberOfDaysInMonth.java
@@ -0,0 +1,11 @@
+public class NumberOfDaysInMonth {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
+        System.out.println(isLeapYear(2004));
+
+
+    }
+}
