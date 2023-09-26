8649872e1ac2248231097af2820dfafd67492b90
Kacper Kornas <kornaskacper1@gmail.com>
2023-09-26 21:43:40 +0200

    Finally for statement

--- Section6ControlFlow/LearnWithMovieAndCodeChallange/src/NumberOfDaysInMonth.java
+++ Section6ControlFlow/LearnWithMovieAndCodeChallange/src/NumberOfDaysInMonth.java
@@ -1,13 +1,47 @@
 public class NumberOfDaysInMonth {
     public static void main(String[] args) {
-//        System.out.println(isLeapYear(-1600));
-//        System.out.println(isLeapYear(1600));
-//        System.out.println(isLeapYear(2017));
-//        System.out.println(isLeapYear(2000));
-//        System.out.println(isLeapYear(2004));
+        System.out.println(getDaysInMonth(1, 1900));
+        System.out.println(getDaysInMonth(4, 2000));
+        System.out.println(getDaysInMonth(2, 1900));
+        System.out.println(getDaysInMonth(2, 2000));
 
 
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else if (year % 100 == 0 && year % 400 == 0) {
+            return true;
+        } else if (year % 4 == 0 && year % 100 == 0) {
+            return false;
+        } else {
+            return year % 4 == 0;
         }
     }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12) {
+            return -1;
+        } else if (year < 1 || year > 9999) {
+            return -1;
+        } else if (isLeapYear(year)) {
+            int days = switch (month) {
+                case 1, 3, 5, 7, 8, 10, 12 -> 31;
+                case 2 -> 29;
+                default -> 30;
+            };
+            return days;
+        } else {
+            int days = switch (month) {
+                case 1, 3, 5, 7, 8, 10, 12 -> 31;
+                case 2 -> 28;
+                default -> 30;
+            };
+            return days;
+        }
+    }
+}
+
 
 
