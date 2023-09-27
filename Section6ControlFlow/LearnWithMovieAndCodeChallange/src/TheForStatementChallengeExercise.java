04d34fc82ecab17b5153fef1223f0769b2a08b5d
Kacper Kornas <kornaskacper1@gmail.com>
2023-09-27 22:28:05 +0200

    for loop again

--- Section6ControlFlow/LearnWithMovieAndCodeChallange/src/TheForStatementChallengeExercise.java
+++ Section6ControlFlow/LearnWithMovieAndCodeChallange/src/TheForStatementChallengeExercise.java
@@ -1,9 +1,41 @@
 public class TheForStatementChallengeExercise {
+
     public static void main(String[] args) {
 
-        for (int counter = 1; counter <= 5; counter++) {
-            System.out.println(counter);
+//        primeCounter(1,10);
+//        primeCounter(25,100);
+        primeCounter(1,1000);
+    }
+
+    
+    public static boolean isPrime(int wholeNumber) {
+
+        if (wholeNumber < 2) {
+            return false;
         }
 
+        for (int divisor = 2; divisor <= wholeNumber / 2; divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void primeCounter(int valueStart, int end) {
+
+        int primeNumberCounter = 0;
+
+        for (int start = valueStart; start <= end; start++) {
+            if (isPrime(start)) {
+                primeNumberCounter++;
+                System.out.println("Founded prime number = " + start + " Amount of finding prime numbers = " + primeNumberCounter);
+                if (primeNumberCounter == 3) {
+                    break;
+                }
+            }
+        }
     }
 }
+
