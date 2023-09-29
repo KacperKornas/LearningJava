60fadeabad952de862db6263a5751e79f50e9b7d
Kacper Kornas <kornaskacper1@gmail.com>
2023-09-29 14:53:46 +0200

    do while loop

--- Section6ControlFlow/LearnWithMovieAndCodeChallange/src/DigitSumChallenge.java
+++ Section6ControlFlow/LearnWithMovieAndCodeChallange/src/DigitSumChallenge.java
@@ -1,4 +1,24 @@
 public class DigitSumChallenge {
 
-    
+    public static void main(String[] args) {
+        System.out.println(sumDigits(-12));
+        System.out.println(sumDigits(6));
+        System.out.println(sumDigits(33));
+        System.out.println(sumDigits(167));
+        System.out.println(sumDigits(1674));
+        System.out.println(sumDigits(1111111111));
+    }
+
+    public static int sumDigits(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int sum = 0;
+        while (number > 0){
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
 }
