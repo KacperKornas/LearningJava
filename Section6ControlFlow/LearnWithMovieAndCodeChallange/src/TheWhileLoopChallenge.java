6f50b029add70673dc7941d317121922bf8c50de
Kacper Kornas <kornaskacper1@gmail.com>
2023-09-29 09:34:49 +0200

    digit sum challange

--- Section6ControlFlow/LearnWithMovieAndCodeChallange/src/TheWhileLoopChallenge.java
+++ Section6ControlFlow/LearnWithMovieAndCodeChallange/src/TheWhileLoopChallenge.java
@@ -1,8 +1,31 @@
 public class TheWhileLoopChallenge {
 
+
     public static void main(String[] args) {
 
+        int start = 4;
+        int end = 20;
+        int allEvenNumbers = 0;
+        int allOddNumbers = 0;
+
+        while (start <= end) {
+            start++;
+            if (!isEvenNumber(start)) {
+                allOddNumbers++;
+                continue;
+            }
+            allEvenNumbers++;
+            System.out.println(start + " is even number");
+            if (allEvenNumbers == 5) {
+                break;
+            }
+        }
+        System.out.println("Total founded even numbers = " + allEvenNumbers);
+        System.out.println("Total founded odd numbers = " + allEvenNumbers);
     }
 
-    
+
+    public static boolean isEvenNumber(int number) {
+        return number % 2 == 0;
+    }
 }
