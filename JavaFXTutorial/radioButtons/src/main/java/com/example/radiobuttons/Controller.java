9dbb37a0543a942462c2ad854460d5485d9d35bf
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2024-09-11 20:08:06 +0200

    radio buttons

---
+++ JavaFXTutorial/radioButtons/src/main/java/com/example/radiobuttons/Controller.java
@@ -0,0 +1,24 @@
+package com.example.radiobuttons;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
+public class Controller {
+
+    @FXML
+    RadioButton radio1, radio2, radio3;
+    @FXML
+    Label myLabel;
+
+    public void select() {
+        
+        if (radio1.isSelected()) {
+            myLabel.setText("pizza");
+        } else if (radio2.isSelected()) {
+            myLabel.setText("sushi");
+        } else if (radio3.isSelected()) {
+            myLabel.setText("ramen");
+        }
+    }
+}
