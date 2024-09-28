397944b42e2a38281febdc903819e455b587255c
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2024-09-28 19:16:59 +0200

    mp3 music player

---
+++ JavaFXTutorial/mp3MusicPlayer/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.mp3musicplayer {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.mp3musicplayer to javafx.fxml;
+    exports com.example.mp3musicplayer;
+}
