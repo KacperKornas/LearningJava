9619f43b807fd2ebf82457f8ba54b6369b5fba5b
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2024-11-12 16:33:09 +0100

    second task

---
+++ JavaInterviewQuestions/src/BinarySearchRecursive.java
@@ -0,0 +1,8 @@
+public class BinarySearchRecursive {
+    public static void main(String[] args) {
+        int[] arr = {2,5,14,16,30,46,60,70,83,92};
+        int target = 14;
+
+        
+    }
+}
