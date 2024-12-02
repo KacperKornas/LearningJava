b4855a1811c4b36336047613488d13177bf81738
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2024-12-02 16:25:10 +0100

    Lets start another book

---
+++ JavaPrzewodnikDlaPoczatkujacych/src/GalToLit.java
@@ -0,0 +1,9 @@
+public class GalToLit {
+    public static void main(String[] args) {
+
+        double gal = 10;
+        double lit = gal * 3.7854;
+
+        System.out.println(gal + " galonów odpowiada " + lit + " litrom.");
+    }
+}
