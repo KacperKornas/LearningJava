55a60f0720a5fdc514eda4b95d2c415a364b453c
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2024-12-04 20:23:13 +0100

    Create Sound.java

---
+++ JavaPrzewodnikDlaPoczatkujacych/src/Sound.java
@@ -0,0 +1,7 @@
+public class Sound {
+    public static void main(String[] args) {
+        double time = 7.2 / 2;
+        double distance = time * 330;
+        System.out.println(distance);
+    }
+}
