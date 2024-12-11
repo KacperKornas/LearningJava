f4e8d9e4bbc9aab27737b378eefe81191dd1d8cc
KacperKornas <kornaskacper1@gmail.com>
2024-12-11 20:02:19 +0100

    Update PrimeNumber.java

--- JavaPrzewodnikDlaPoczatkujacych/src/PrimeNumber.java
+++ JavaPrzewodnikDlaPoczatkujacych/src/PrimeNumber.java
@@ -1,10 +1,18 @@
 public class PrimeNumber {
     public static void main(String[] args) {
 
-        for(int i = 2; i<=100; i++) {
-            if (i%2 == 0) {
-                System.out.println(i);
+        for (int i = 2; i <= 100; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
             }
+            if (isPrime) {
+                System.out.println(i);
+            };
         }
     }
 }
