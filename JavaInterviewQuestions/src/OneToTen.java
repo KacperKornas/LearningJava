b57556c61f0d467d18e225d998609ed71321e9ca
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2024-12-14 21:09:58 +0100

    little task

---
+++ JavaInterviewQuestions/src/OneToTen.java
@@ -0,0 +1,9 @@
+public class OneToTen {
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 11; i++) {
+            if (i == 10) System.out.println(i);
+            else System.out.print(i + ",");
+        }
+    }
+}
