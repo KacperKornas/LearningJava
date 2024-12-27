ff85da96a00f58a6297097bb4846c266888b6996
KacperKornas <kornaskacper1@gmail.com>
2024-12-27 09:25:45 +0100

    Create Test3E4.java

---
+++ JavaPrzewodnikDlaPoczatkujacych/src/Test3E4.java
@@ -0,0 +1,10 @@
+/*Napisz pętlę for, której zmienna sterująca przyjmuje wartości
+ od 1000 do 0 z krokiem równymm -2 */
+
+public class Test3E4 {
+   public static void main(String[] args) {
+       for(int i = 1000; i >= 0; i -= 2) {
+           System.out.println(i);
+       }
+   }
+}
