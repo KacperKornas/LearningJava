71335cfeeb2af5a16dd6c7926e1035df4fc8b1e8
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2024-12-30 19:00:56 +0100

    Create Test4E4.java

---
+++ JavaPrzewodnikDlaPoczatkujacych/src/Test4E4.java
@@ -0,0 +1,9 @@
+public class Test4E4 {
+    public static void main(String[] args) {
+        MyCounter counter;
+        counter = new MyCounter();
+    }
+    
+    static class MyCounter {
+    }
+}
