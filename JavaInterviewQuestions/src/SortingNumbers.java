539d95442f416c1f03adbfe9436251e9aedebaf8
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2025-01-08 12:56:27 +0100

    Update SortingNumbers.java

--- JavaInterviewQuestions/src/SortingNumbers.java
+++ JavaInterviewQuestions/src/SortingNumbers.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class SortingNumbers {
+    public static void main(String[] args) {
+        int [] numbers = new int[8];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 8; i++) {
+            System.out.println("Give me a number: ");
+            numbers[i] = (scanner.nextInt());
+        }
+
+        
+
+        for (int i : numbers) {
+            System.out.print(i + " ");
+        }
+    }
 }
