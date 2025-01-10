af6b01ed87a9615e89289d049356f8f2105f7aff
KacperKornas <kornaskacper1@gmail.com>
2025-01-10 19:30:20 +0100

    Update SortingNumbers.java

--- JavaInterviewQuestions/src/SortingNumbers.java
+++ JavaInterviewQuestions/src/SortingNumbers.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class SortingNumbers {
     public static void main(String[] args) {
         int [] numbers = new int[8];
+        int value;
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < 8; i++) {
@@ -10,7 +11,21 @@ public class SortingNumbers {
             numbers[i] = (scanner.nextInt());
         }
 
-        
+        for (int i : numbers) {
+            System.out.print(i + " ");
+        }
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (numbers[j] > numbers[i]) {
+                        value = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = value;
+                }
+            }
+        }
+
+        System.out.println();
 
         for (int i : numbers) {
             System.out.print(i + " ");
