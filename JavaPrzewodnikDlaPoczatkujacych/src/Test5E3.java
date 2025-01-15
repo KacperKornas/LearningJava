e2df957d3a93455543d0270ffe1ca544f11c9647
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2025-01-15 08:43:14 +0100

    Create Test5E3.java

---
+++ JavaPrzewodnikDlaPoczatkujacych/src/Test5E3.java
@@ -0,0 +1,12 @@
+public class Test5E3 {
+    public static void main(String[] args) {
+        double[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        double sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("The avrerage is: " + sum / arr.length);
+    }
+}
