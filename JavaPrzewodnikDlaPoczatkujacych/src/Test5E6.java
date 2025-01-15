6e889612b679096107be9979f989f46d8c1ab2a8
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2025-01-15 09:52:33 +0100

    Create Test5E6.java

---
+++ JavaPrzewodnikDlaPoczatkujacych/src/Test5E6.java
@@ -0,0 +1,8 @@
+public class Test5E6 {
+    public static void main(String[] args) {
+        String str = "Lubię Javę";
+
+        System.out.println(str.length());
+        System.out.println(str.charAt(6));
+    }
+}
