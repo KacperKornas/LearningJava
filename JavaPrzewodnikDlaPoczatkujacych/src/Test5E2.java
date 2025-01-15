859bad5807323e798aebd59b871bcaa1383a0247
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2025-01-15 08:33:12 +0100

    Create Test5E2.java

---
+++ JavaPrzewodnikDlaPoczatkujacych/src/Test5E2.java
@@ -0,0 +1,14 @@
+public class Test5E2 {
+    public static void main(String[] args) {
+
+        int[] arr = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+}
