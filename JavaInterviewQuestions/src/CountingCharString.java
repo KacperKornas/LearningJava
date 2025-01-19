4263bc1d159d6e242fe8eacb95c52999e01931c0
KacperKornas <kornaskacper1@gmail.com>
2025-01-19 20:51:20 +0100

    Create CountingCharString.java

---
+++ JavaInterviewQuestions/src/CountingCharString.java
@@ -0,0 +1,20 @@
+public class CountingCharString {
+    public static void main(String[] args) {
+        CountingChar counter = new CountingChar();
+
+        System.out.println(counter.counting("Ala ma kota", 'a'));
+    }
+}
+
+class CountingChar {
+
+    int counting(String str, char ch) {
+        int counter = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) counter++;
+        }
+        return counter;
+    }
+}
+
+
