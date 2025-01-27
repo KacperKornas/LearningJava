49dbff5bcd115fa8e1cc2686b49ff0baeec5091f
KacperKornas <kornaskacper1@gmail.com>
2025-01-27 10:56:18 +0100

    Update Test6E3.java

--- JavaPrzewodnikDlaPoczatkujacych/src/Chapter6/Test6E3.java
+++ JavaPrzewodnikDlaPoczatkujacych/src/Chapter6/Test6E3.java
@@ -1,4 +1,26 @@
 package Chapter6;
 
 public class Test6E3 {
+    public static void main(String[] args) {
+        Stack stack = new Stack(4);
+
+        for (int i : stack.arr) {
+            System.out.println(stack.arr[i]);
+        }
+
+    }
+}
+
+class Stack {
+    int[] arr;
+    int top;
+
+    public Stack(int size) {
+        arr = new int[size];
+        top = - 1;
+    }
+
+//    void pop() {
+//        arr -= arr[arr.length-1];
+//    }
 }
