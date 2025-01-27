f3578670aa7a5d7129c045c9499961716ac4fe73
KacperKornas <kornaskacper1@gmail.com>
2025-01-27 11:48:57 +0100

    Update Test6E3.java

--- JavaPrzewodnikDlaPoczatkujacych/src/Chapter6/Test6E3.java
+++ JavaPrzewodnikDlaPoczatkujacych/src/Chapter6/Test6E3.java
@@ -4,6 +4,11 @@ public class Test6E3 {
     public static void main(String[] args) {
         Stack stack = new Stack(4);
 
+        stack.push(2);
+        stack.push(5);
+//        stack.push(8);
+//        System.out.println(stack.top);
+
         for (int i : stack.arr) {
             System.out.println(stack.arr[i]);
         }
@@ -17,9 +22,17 @@ class Stack {
 
     public Stack(int size) {
         arr = new int[size];
-        top = - 1;
+        top = -1;
     }
 
+    void  push(int value) {
+        if(top > arr.length - 1) {
+            System.out.println("Stack overflow!");
+        } else {
+            top++;
+            arr[top] = value;
+        }
+    }
 //    void pop() {
 //        arr -= arr[arr.length-1];
 //    }
