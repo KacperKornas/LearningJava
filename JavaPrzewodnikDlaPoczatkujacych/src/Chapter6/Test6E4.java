601a4fbba50616e96d7d4c786d8b96f4b5e240e5
KacperKornas <kornaskacper1@gmail.com>
2025-01-31 15:27:08 +0100

    Update Test6E4.java

--- JavaPrzewodnikDlaPoczatkujacych/src/Chapter6/Test6E4.java
+++ JavaPrzewodnikDlaPoczatkujacych/src/Chapter6/Test6E4.java
@@ -1,4 +1,26 @@
 package Chapter6;
 
 public class Test6E4 {
+    public static void main(String[] args) {
+        Test a = new Test(7);
+        Test b = new Test(2);
+
+        System.out.println(a.a);
+        System.out.println(b.a);
+        a.swap(a,b);
+        System.out.println(a.a);
+        System.out.println(b.a);
+    }
+}
+
+class Test {
+    int a;
+    Test(int i) { a = i; }
+
+    void swap(Test obj1, Test obj2) {
+        Test change = new Test(0);
+        change.a = obj1.a;
+        obj1.a = obj2.a;
+        obj2.a = change.a;
+    }
 }
