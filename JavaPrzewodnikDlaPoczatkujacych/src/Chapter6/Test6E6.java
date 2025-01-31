2fb335e45eeed5a160ae1db576eb745ed56bdd5a
KacperKornas <kornaskacper1@gmail.com>
2025-01-31 16:31:44 +0100

    Update Test6E6.java

--- JavaPrzewodnikDlaPoczatkujacych/src/Chapter6/Test6E6.java
+++ JavaPrzewodnikDlaPoczatkujacych/src/Chapter6/Test6E6.java
@@ -4,15 +4,14 @@ public class Test6E6 {
     public static void main(String[] args) {
         Test6E6 test = new Test6E6();
 
-        test.showBackWard("Show me everything.", 0);
+        test.showBackWard("Show me everything.");
     }
 
-    void showBackWard(String str, int index) {
-        if (index == str.length())
+    void showBackWard(String str) {
+        if (str.isEmpty())
             return;
 
-        showBackWard(str, index + 1);
-
-        System.out.print(str.charAt(str.length() - index - 1));
+        showBackWard(str.substring(1));
+        System.out.print(str.charAt(0));
     }
 }
