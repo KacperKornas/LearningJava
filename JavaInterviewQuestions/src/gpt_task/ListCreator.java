be10b07683ebb77bcc616a14c01a932a25d65eb2
KacperKornas <kornaskacper1@gmail.com>
2025-02-12 20:33:43 +0100

    Update ListCreator.java

--- JavaInterviewQuestions/src/gpt_task/ListCreator.java
+++ JavaInterviewQuestions/src/gpt_task/ListCreator.java
@@ -1,13 +1,31 @@
 package gpt_task;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ListCreator {
 
+    String text;
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+
     public void enterNumbers() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give me numbers separated by ' '.");
-        String text = scanner.nextLine();
+        text = scanner.nextLine();
+    }
+
+    public void listCreator() {
+        int a;
+        for (int i = 0; i < text.length(); i += 2) {
+            a = text.charAt(i);
+            numbers.add(a);
+        }
+    }
+
+    public void showList() {
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
     }
 
 
@@ -16,5 +34,7 @@ public class ListCreator {
     public static void main(String[] args) {
         ListCreator list = new ListCreator();
         list.enterNumbers();
+        list.listCreator();
+        list.showList();
     }
 }
