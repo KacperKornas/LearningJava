8e44bc53e9d2344604a5f7a921d3399253078b3d
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2025-04-07 18:04:09 +0200

    Update CheckingAnagrams.java

--- JavaInterviewQuestions/src/CheckingAnagrams.java
+++ JavaInterviewQuestions/src/CheckingAnagrams.java
@@ -1,2 +1,30 @@
+import java.util.*;
+
 public class CheckingAnagrams {
+    public static void main(String[] args) {
+        CheckingAnagrams check = new CheckingAnagrams();
+        String str1 = "listen";
+        String str2 = "silent";
+
+        check.areAnagrams(str1, str2);
+        if (check.areAnagrams(str1, str2)) System.out.println(str1 + " and " + str2 + " are anagrams!");
+         else System.out.println(str1 + " and " + str2 + " aren't anagrams!");
+    }
+
+    boolean areAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+
+        Set<Character> sList1 = new TreeSet<Character>();
+        Set<Character> sList2 = new TreeSet<Character>();
+
+        for (int i = 0; i <= str1.length() - 1; i++) {
+            sList1.add(str1.charAt(i));
+        }
+
+        for (int i = 0; i <= str2.length() - 1; i++) {
+            sList2.add(str2.charAt(i));
+        }
+
+        return sList1.equals(sList2);
+    }
 }
