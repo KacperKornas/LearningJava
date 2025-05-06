e6a231fa221dd8445a7f430be0febd278be26c71
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2025-05-06 22:08:03 +0200

    tests for non repeating character

---
+++ JavaInterviewQuestions/src/test/java/gpt/task/FirstNonRepeatingCharacterTest.java
@@ -0,0 +1,18 @@
+package gpt.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstNonRepeatingCharacterTest {
+
+    @Test
+    void testFirstNonRepeating() {
+        FirstNonRepeatingCharacter nonR = new FirstNonRepeatingCharacter();
+        assertEquals('l', FirstNonRepeatingCharacter.findNonRepeat("leetcode"));
+        assertEquals(' ', FirstNonRepeatingCharacter.findNonRepeat("aabb"));
+        assertEquals('c', FirstNonRepeatingCharacter.findNonRepeat("aabbc"));
+        assertEquals(' ', FirstNonRepeatingCharacter.findNonRepeat(""));
+        assertEquals('l', FirstNonRepeatingCharacter.findNonRepeat("l"));
+    }
+}
