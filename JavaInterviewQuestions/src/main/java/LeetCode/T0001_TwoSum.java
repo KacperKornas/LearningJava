c9f8de4626d0901d12e0d58e832ba011a6031226
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2025-05-15 18:07:23 +0200

    Create T0001_TwoSum.java

---
+++ JavaInterviewQuestions/src/main/java/LeetCode/T0001_TwoSum.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class T0001_TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = nums.length - 1; j > i; j--)
+                if(nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
