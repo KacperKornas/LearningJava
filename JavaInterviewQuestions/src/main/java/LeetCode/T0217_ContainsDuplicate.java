e62c534f19c2780a49abf6cd4cd6bc378db911cb
KacperKornas <kornaskacper1@gmail.com>
2025-05-17 20:13:16 +0200

    Update T0217_ContainsDuplicate.java

--- JavaInterviewQuestions/src/main/java/LeetCode/T0217_ContainsDuplicate.java
+++ JavaInterviewQuestions/src/main/java/LeetCode/T0217_ContainsDuplicate.java
@@ -1,11 +1,15 @@
 package LeetCode;
 
+import java.util.HashSet;
+
 public class T0217_ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) return true;
-            }
+        HashSet<Integer> seen = new HashSet<>();
+
+        for(int num : nums) {
+            if (seen.contains(num)) return true;
+
+            seen.add(num);
         }
         return false;
     }
