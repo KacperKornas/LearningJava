a2bdc3b134d6c63f5288c5c00126ac7269da6c74
KacperKornas <kornaskacper1@gmail.com>
2025-05-18 14:11:15 +0200

    Counting Bits

--- JavaInterviewQuestions/src/main/java/LeetCode/T0191_NumberOf1Bits.java
+++ JavaInterviewQuestions/src/main/java/LeetCode/T0191_NumberOf1Bits.java
@@ -1,14 +1,20 @@
 package LeetCode;
 
 public class T0191_NumberOf1Bits {
-    public int hammingWeight(int n) {
-        String binaryString = Integer.toBinaryString(n);
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
         int hammingWeight = 0;
-        for(int i = 0; i < binaryString.length(); i++) {
-            if(binaryString.charAt(i) == '1') {
-                hammingWeight++;
+
+        for(int i = 0; i <= n; i++) {
+            String binaryNumber = Integer.toBinaryString(i);
+            for(int j = 0; j < binaryNumber.length(); j++) {
+                if(binaryNumber.charAt(j) == '1') {
+                    hammingWeight++;
+                }
             }
+            ans[i] = hammingWeight;
+            hammingWeight = 0;
         }
-        return hammingWeight;
+        return ans;
     }
 }
