76f1ec7d005259e3f934516dd803d63db5cca2d9
KacperKornas <kornaskacper1@gmail.com>
2025-05-18 15:41:08 +0200

    Create T0268_MissingNumber.java

---
+++ JavaInterviewQuestions/src/main/java/LeetCode/T0268_MissingNumber.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+import java.util.ArrayList;
+
+public class T0268_MissingNumber {
+    public int missingNumber(int[] nums) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int num : nums) {
+            numbers.add(num);
+        }
+
+        int missing = nums.length;
+
+        for(int i = 0; i < numbers.size(); i++) {
+            if(!numbers.contains(i)) {
+                missing = i;
+                break;
+            }
+        }
+        return missing;
+    }
+}
