e536d4b537946305f40706325177dcd1e008da70
KacperKornas <kornaskacper1@gmail.com>
2025-05-19 16:01:43 +0200

    Create T0190_ReverseBits.java

---
+++ JavaInterviewQuestions/src/main/java/LeetCode/T0190_ReverseBits.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+public class T0190_ReverseBits {
+    public int reverseBits(int n) {
+        int result = 0;
+        for(int i = 0; i < 32; i++) {
+            result <<= 1;
+            result |= (n & 1);
+            n >>>= 1;
+        }
+        return result;
+    }
+}
