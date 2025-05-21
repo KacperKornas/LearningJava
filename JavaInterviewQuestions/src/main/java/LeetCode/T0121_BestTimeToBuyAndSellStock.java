be744f821efeb13234b54b39f7e420a65a56315b
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2025-05-21 19:21:06 +0200

    Update T0121_BestTimeToBuyAndSellStock.java

--- JavaInterviewQuestions/src/main/java/LeetCode/T0121_BestTimeToBuyAndSellStock.java
+++ JavaInterviewQuestions/src/main/java/LeetCode/T0121_BestTimeToBuyAndSellStock.java
@@ -1,16 +1,14 @@
 package LeetCode;
 
 public class T0121_BestTimeToBuyAndSellStock {
-    public int maxProfit(int[] prices) {
-        int minPrice = Integer.MAX_VALUE;
-        int bestProfit = 0;
+    public boolean isPalindrome(String s) {
+        String converted = s.replaceAll("[^a-zA-Z0-9]", "");
+        converted = converted.toLowerCase();
 
-        for(int price : prices) {
-            if(price < minPrice) {
-                minPrice = price;
-            } else if (price - minPrice > bestProfit) {
-                bestProfit = price - minPrice;
-            }
-        } return bestProfit;
+        for (int i = 0; i < converted.length() / 2; i++) {
+            if (converted.charAt(i) == converted.charAt(converted.length() - i - 1)) continue;
+            return false;
+        }
+        return true;
     }
 }
