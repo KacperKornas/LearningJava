8e36abd7259909d3d9f6ce2909e880c947e48b62
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2025-05-22 21:15:42 +0200

    Update T0206_ReverseLinkedList.java

--- JavaInterviewQuestions/src/main/java/LeetCode/T0206_ReverseLinkedList.java
+++ JavaInterviewQuestions/src/main/java/LeetCode/T0206_ReverseLinkedList.java
@@ -1,6 +1,14 @@
 package LeetCode;
 
 public class T0206_ReverseLinkedList {
+      static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode current = head;
