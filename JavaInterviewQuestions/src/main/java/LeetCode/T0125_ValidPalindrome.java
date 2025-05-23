01f4b01d54ea3a8d33f4846eabbef460b5297e7f
KacperKornas <kornaskacper1@gmail.com>
2025-05-23 16:55:48 +0200

    Update T0125_ValidPalindrome.java

--- JavaInterviewQuestions/src/main/java/LeetCode/T0125_ValidPalindrome.java
+++ JavaInterviewQuestions/src/main/java/LeetCode/T0125_ValidPalindrome.java
@@ -1,4 +1,14 @@
 package LeetCode;
 
 public class T0125_ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        String respond = s.replaceAll("[^a-zA-Z0-9]", "");
+        respond = respond.toLowerCase();
+
+        for(int i = 0; i < s.length()/2; i++) {
+            if (respond.charAt(i) == respond.charAt(respond.length() - i - 1)) continue;
+            return false;
+        }
+        return true;
+    }
 }
