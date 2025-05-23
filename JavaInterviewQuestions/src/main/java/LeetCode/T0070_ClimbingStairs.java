3cf0068971847df1e12c6b7a380f1d6fe7481176
KacperKornas <kornaskacper1@gmail.com>
2025-05-23 17:21:06 +0200

    Update T0070_ClimbingStairs.java

--- JavaInterviewQuestions/src/main/java/LeetCode/T0070_ClimbingStairs.java
+++ JavaInterviewQuestions/src/main/java/LeetCode/T0070_ClimbingStairs.java
@@ -1,5 +1,17 @@
 package LeetCode;
 
 public class T0070_ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n < 2) return n;
 
+        int first = 1;
+        int second = 2;
+
+        for(int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
 }
