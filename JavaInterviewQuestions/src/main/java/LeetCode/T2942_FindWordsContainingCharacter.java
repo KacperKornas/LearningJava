49f1850dd96931ac831248ae3408ff55d59e172c
KacperKornas <kornaskacper1@gmail.com>
2025-05-24 14:36:05 +0200

    Create T2942_FindWordsContainingCharacter.java

---
+++ JavaInterviewQuestions/src/main/java/LeetCode/T2942_FindWordsContainingCharacter.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class T2942_FindWordsContainingCharacter {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        ArrayList<Integer> respond = new ArrayList<>();
+
+        for(int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                if(x == words[i].charAt(j)) {
+                    respond.add(i);
+                    break;
+                }
+            }
+        }
+        return respond;
+    }
+}
