06bcd4a15087dbe5570840bf1566687d0efedc14
KacperKornas <116450906+KacperKornas@users.noreply.github.com>
2025-06-30 10:44:42 +0200

    Create T0026_RemoveDuplicatesFromSortedArray.java

---
+++ JavaInterviewQuestions/src/main/java/LeetCode/T0026_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+public class T0026_RemoveDuplicatesFromSortedArray {
+    public static int removeDuplicates(int[] nums) {
+        int slow = 1;
+
+        for (int fast = 1; fast < nums.length; fast++) {
+            if(nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
